9609f13bbe0e07c4f451cdce2392f93c9158421e
Kevin Cruz <kevin_mcp@outlook.com>
2021-06-15 13:42:06 -0400

    Home page finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
           {latestEpisodes.map(episode => {
             return (
               <li key={episode.id}>
-                <Image width={192} height={192} src={episode.thumbnail} alt={episode.title} objectFit='cover'/>
+                <Image width={192} height={192} src={episode.thumbnail} alt={episode.title} objectFit='cover' />
 
                 <div className={styles.episodeDetails}>
                   <a href="">{episode.title}</a>
@@ -55,7 +55,48 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
       </section>
 
       <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
 
+        <table cellSpacing={0}>
+          <thead>
+            <th></th>
+            <th>Podcast</th>
+            <th>Integrantes</th>
+            <th>Data</th>
+            <th>Duração</th>
+            <th></th>
+          </thead>
+          <tbody>
+            {allEpisodes.map(episode => {
+              return (
+                <tr key={episode.id}>
+                  <td style={{width:92}}>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit='cover'
+                    />
+                  </td>
+                  <td>
+                    <a href="">{episode.title}</a>
+                  </td>
+                  <td>{episode.members}</td>
+                  <td style={{width:120}}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button type='button'>
+                        <img src="/play-green.svg" alt="Tocar episódio" />
+                    </button>
+                  </td>
+
+                </tr>
+              )
+            })}
+          </tbody>
+
+        </table>
       </section>
     </div>
   )
